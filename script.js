86db1c85dc3d42ddaa7ff339045ddce3479485cc
Khojodelly <abugribismark4@gmail.com>
2026-01-29 12:41:58 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-const orderBtn = document.getElementById("orderBtn");
-
-orderBtn.addEventListener("click", function () {
-    const phoneNumber = "233552820935";
-    const message = "Hello ESTEESBITES, I would like to place an order.";
-
-    const whatsappLink = 
-        "https://wa.me/" + phoneNumber + "?text=" + encodeURIComponent(message);
-    window.open(whatsappLink, "_blank");
-});
 
 const navbar = document.getElementById("navbar");
 
@@ -19,36 +9,6 @@ window.addEventListener("scroll", function () {
     }
 });
 
-const form = document.getElementById("orderForm");
-
-form.addEventListener("submit", function (e) {
-    e.preventDefault();
-
-    const name = document.getElementById("name").value.trim();
-    const phone = document.getElementById("phone").value.trim();
-    const location = document.getElementById("location").value.trim();
-    const message1 = document.getElementById("message1").value.trim();
-
-    if (name === ""|| phone === ""||location ===""|| message1 === "") {
-        alert("Please fill in all fields");
-        return;
-    }
-
-    const fullMessage =
-        "Name: " + name +
-        "\nPhone: " + phone +
-        "\nLocation: "+ location +
-        "\nOrder: " + message1;
-
-    const whatsappNumber = "233552820935";
-    const whatsappURL =
-        "https://wa.me/" +
-        whatsappNumber +
-        "?text=" +
-        encodeURIComponent(fullMessage);
-
-    window.open(whatsappURL, "_blank");
-});
 
 const sections = document.querySelectorAll("section");
 
@@ -85,4 +45,5 @@ navItems.forEach(item => {
         menuToggle.textContent = "☰";
     });
 });
+
 
